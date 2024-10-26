58d21af5b5ba0df7fe503280aa9c77566df89be4
dharshanspn <86490616+dharshanspn@users.noreply.github.com>
2024-10-26 23:00:37 +0530

    Update script.py

--- script.py
+++ script.py
@@ -15,7 +15,7 @@ accounts = [
     {"username": "takeadpay@gmail.com", "password": "Mail@202", "user_bot_chatID": '7091969636', "account_name": "pusph", "user_bot_token" : "8131045025:AAE9_BMb5i2pk479mubtilbSIUilPA25jWM"},
     {"username": "sakshichoudharysak31@gmail.com", "password": "ihsk@S#31", "user_bot_chatID": '1125920838', "account_name": "Avnish", "user_bot_token" : "8131045025:AAE9_BMb5i2pk479mubtilbSIUilPA25jWM"},
     {"username": "freelancing1725@gmail.com", "password": "Chegg@1725", "user_bot_chatID": '6059954492', "account_name": "Fasil", "user_bot_token" : "8131045025:AAE9_BMb5i2pk479mubtilbSIUilPA25jWM"},
-    #{"username": "rahiabrafiq152@gmail.com", "password": "Rent@007", "user_bot_chatID": '1155462778', "account_name": "Umeed", "user_bot_token" : "8131045025:AAE9_BMb5i2pk479mubtilbSIUilPA25jWM"},
+    {"username": "lokeshyadav8104@gmail.com", "password": "Yadav@123", "user_bot_chatID": '1155462778', "account_name": "Umeed", "user_bot_token" : "8131045025:AAE9_BMb5i2pk479mubtilbSIUilPA25jWM"},
     # Add more accounts if necessary
 ]
 
